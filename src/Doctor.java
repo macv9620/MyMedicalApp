4c1bf7eb4a54a0172a637bfac03f15e4f8e07742
Mateo Vasco <macv9620@utp.edu.co>
2023-08-13 01:08:42 -0500

    Se crean clases UUMenu y Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,25 @@
+import javax.print.Doc;
+
 public class Doctor {
-    int id;
+    int id = 0;
     String name;
     String speciality;
 
+    Doctor(){
+        System.out.println("Ejecutando constructor");
+    }
+
+    Doctor(String name, String speciality){
+        this.name = name;
+        this.speciality = speciality;
+        id++;
+    }
+
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println(id);
     }
 }
