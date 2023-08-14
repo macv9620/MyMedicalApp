62ea4e2958d90709fe469c2649ac2210310ea1ad
Mateo Vasco <macv9620@utp.edu.co>
2023-08-13 20:32:42 -0500

    Se crean clases anidadas para Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,25 +1,79 @@
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+
 import javax.print.Doc;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Doctor {
-    int id = 0;
-    String name;
-    String speciality;
-
-    Doctor(){
-        System.out.println("Ejecutando constructor");
-    }
+    private int id = 0;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String speciality;
 
     Doctor(String name, String speciality){
         this.name = name;
         this.speciality = speciality;
-        id++;
     }
 
-    public void showName(){
-        System.out.println(name);
+    public String getName() {
+        return name;
     }
 
-    public void showId(){
-        System.out.println(id);
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    ArrayList<AvailableAppointments> availableDates = new ArrayList<>();
+    public void addAvailableDate(Date date, String time){
+        availableDates.add(new AvailableAppointments(date, time));
+    }
+
+    public ArrayList<AvailableAppointments> getAvailableDates(){
+        return availableDates;
+    };
+
+    public static class AvailableAppointments{
+        private int id;
+        private Date date;
+        private String time;
+
+        AvailableAppointments(Date date, String time){
+            this.date = date;
+            this.time = time;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
-    public static void main (String[] args){
-        //showMenu();
+    public static void main (String[] args) {
+        //Al ser public se puede acceder directamente al método setter únicamente instanciando la clase
 
-        Doctor myDoctor = new Doctor("Holi", "Jeje");
+        Doctor myDoctor = new Doctor("Mateo Vasco", "Internista");
+        myDoctor.addAvailableDate(new Date(), "2pm");
+        myDoctor.addAvailableDate(new Date(), "5pm");
+        myDoctor.addAvailableDate(new Date(), "9pm");
+
+        System.out.println("Available dates for Dr: " + myDoctor.getName());
+        for(Doctor.AvailableAppointments avAppointment : myDoctor.getAvailableDates()){
+            System.out.println(avAppointment.getDate() + " " + avAppointment.getTime());
+        }
 
     }
 }
